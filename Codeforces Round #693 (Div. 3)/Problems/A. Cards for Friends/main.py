c89f634a03d75453e62956a139bc2696796ff90e
Atsocs <atsocs.cc@gmail.com>
2021-02-25 07:09:38 -0300

    1475 | A accepted

--- Codeforces Round #693 (Div. 3)/Problems/A. Cards for Friends/main.py
+++ Codeforces Round #693 (Div. 3)/Problems/A. Cards for Friends/main.py
@@ -2,6 +2,23 @@
 Accomplished using the EduTools plugin by JetBrains https://plugins.jetbrains.com/plugin/10081-edutools
 """
 
+
+def trailing_zeroes(x):
+    x = "{0:b}".format(int(x))
+    return len(x) - len(x.rstrip('0'))
+
+
+def solve(w, h, n):
+    tw, th = list(map(trailing_zeroes, [w, h]))
+    pieces = 2 ** (tw + th)
+    return n <= pieces
+
+
+def main():
+    for case in range(int(input())):
+        w, h, n = tuple(map(int, input().split()))
+        print('YES' if solve(w, h, n) else 'NO')
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    main()
